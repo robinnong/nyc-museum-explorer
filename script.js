53dacf83bd3aa38acfff21e189d042ae29619dd5
robinnong <robin.nong@hotmail.com>
2020-03-16 17:57:35 -0400

    Added icons to results

--- script.js
+++ script.js
@@ -11,12 +11,12 @@ app.$selectType = $('input[type="radio"]');
 app.$filterList = $('ul');
 
 // DO NOT DELETE!
-app.SOCRATA_API_TOKEN = [REDACTED]
-app.SOCRATA_API_URL =  'https://data.cityofnewyork.us/resource/fn6f-htvy.json' 
-app.GOOGLEMAPS_API_URL = 'https://www.google.com/maps/search/?api=1&query='//search function 
+app.SOCRATA_API_TOKEN = [REDACTED]; 
+app.SOCRATA_API_URL =  'https://data.cityofnewyork.us/resource/fn6f-htvy.json'; 
+app.GOOGLEMAPS_API_URL = 'https://www.google.com/maps/search/?api=1&query=';//search function 
 
 // GLOBALLY DECLARED VARIABLES
-app.citiesArray = ["Brooklyn", "New York", "Queens", "Bronx", "Staten Island"]           
+app.citiesArray = ["Brooklyn", "New York", "Queens", "Bronx", "Staten Island"];           
 app.numCities = app.citiesArray.length - 1;
 
 // VISIBILITY
@@ -32,9 +32,10 @@ app.strArray = [
                 ["Culture", "Cultural", "Leo Baeck", "Yeshiva", "Americas", "Numismatic", "Hispanic", "Tenement", "Madame Tussauds", "Tolerance", "Anne Frank","Garibaldi-Meucci", "Jewish", "Asia", "Chinese", "Nordic", "Museo", "Ukrainian", "Italian", "Jazz", "Indian", "Wave Hill"],
                 ["Children's", "Discovery Times"],
                 [" "]
-                ]
+                ];
 
-app.classArray = ["art", "science", "history", "cultural", "childrens"]
+app.classArray = ["art", "science", "history", "cultural", "childrens"];
+app.classIcons = ["fa-palette", "fa-atom", "fa-landmark", "fa-globe", "fa-child"];
 
 let cityIndex = 0; 
 
@@ -89,7 +90,7 @@ app.getByCity = (borough) => {
 app.assignType = function () { 
     for (let n = 0; n<5; n++) {
         $(app.strArray[n]).each(function(){
-            $(".result-container:contains(" + this + ")").addClass(app.classArray[n]);
+            $(".result-container:contains(" + this + ")").find('.icon').addClass(app.classArray[n]).find('.fas').addClass(app.classIcons[n]);
         })
     }
 }
@@ -129,10 +130,13 @@ app.displayResults = (museums) => {
         const mapQuery = `${app.GOOGLEMAPS_API_URL}+${encoded}`
         const museumHtml = 
             `<div class="result-container">
-                <h3>${name}</h3>
+                <div class="name">
+                    <div class="icon"><i class="fas"></i></div>
+                    <h3>${name}</h3>
+                </div>
                 <p class="address"><i class="fas fa-map-marker-alt" aria-hidden="true"></i> <a href="${mapQuery}" target="_blank">${address}, ${city}, NY </a></p>
                 <a class="tel" href="tel:${tel}"><i class="fas fa-phone" aria-hidden="true"></i> ${tel}</a> 
-                <a class="url" href="${url}" rel="external" target="_blank">Visit Website <i class="fas fa-external-link-alt" aria-hidden="true"></i></a>
+                <a class="url" href="${url}" rel="external" target="_blank">Visit Website</a>
             </div>`; 
         for (let n = 0; n<typeFilter.length; n++) {  
             if (name.includes(typeFilter[n])) { 
