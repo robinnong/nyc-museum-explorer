6ba9feea5daefbe87c6544abecc1ba24f5f9872c
robinnong <robin.nong@hotmail.com>
2020-03-17 01:30:53 -0400

    Resolved issue with duplicate results

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ $('.scroll-up, .filter-bar').hide();
 app.strArray = [ 
                 ["Art", "Galerie", "FIT", "Design", "Studio", "Guggenheim", "Illustration", "Collection", "Drawing", "Brooklyn Museum", "Media", "Moving Image", "Photography", "National Academy", "Nicholas Roerich", "Goethe-Institut", "Dahesh"],
                 ["Science", "Technology", "Transit", "Space", "Skyscraper", "Seaport"], 
-                ["History", "Heritage", "Memorial", "Historic", "Archival", "Archive", "Hall Of Fame", "Police", "Library", "Finance", "Alice Austen", "Defense", "Homestead", "Waterfront", "Museum of the City", "Holocaust", "Wyckoff", "Fire", "Island", "Theodore Roosevelt", "Lighthouse", "Farm", "Mansion", "Manor", "Audubon", "Rose Museum", "Vernon Hotel", "Fraunces", "House", "Jackie Robinson", "Audubon "],
+                ["History", "Heritage", "Memorial", "Historic", "Archival", "Archive", "Hall Of Fame", "Police", "Bowne", "Library", "Finance", "Alice Austen", "Defense", "Homestead", "Waterfront", "Alexander Hamilton", "Museum of the City", "Holocaust", "Fire", "Island", "Valentine-Varian", "Cortlandt", "Theodore Roosevelt", "Lighthouse", "Farm", "Old Stone", "Mansion", "Manor", "Rose Museum", "Vernon Hotel", "Fraunces", "Jackie Robinson", "Audubon "],
                 ["Culture", "Cultural", "Leo Baeck", "Yeshiva", "Americas", "Numismatic", "Hispanic", "Tenement", "Madame Tussauds", "Tolerance", "Anne Frank","Garibaldi-Meucci", "Jewish", "Asia", "Chinese", "Nordic", "Museo", "Ukrainian", "Italian", "Jazz", "Indian", "Wave Hill"],
                 ["Children's", "Discovery Times"],
                 [" "]
@@ -156,10 +156,7 @@ app.init = () => {
     });
     $('.filter-bar').focusin(function () {
         app.$filterList.show();
-    });
-    $('.filter-bar').focusout(function () {
-        app.$filterList.hide();
-    });
+    }); 
     app.$toggleUp.on('click', app.toggleCityUp); 
     app.$toggleDown.on('click', app.toggleCityDown); 
     app.$cityForm.on('submit', function(e) {
