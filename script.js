999e2176237a6263b046d04cc3a2a2676af94e4d
robinnong <robin.nong@hotmail.com>
2020-03-17 17:56:14 -0400

    Refactored code and added pointed cursor to drop down menu

--- script.js
+++ script.js
@@ -11,37 +11,35 @@ app.$selectType = $('input[type="radio"]');
 app.$filterList = $('ul');
 
 // DO NOT DELETE!
-app.SOCRATA_API_TOKEN = [REDACTED]  
+app.SOCRATA_API_TOKEN = [REDACTED]
 app.SOCRATA_API_URL =  'https://data.cityofnewyork.us/resource/fn6f-htvy.json'; 
 app.GOOGLEMAPS_API_URL = 'https://www.google.com/maps/search/?api=1&query=';//search function 
 
 // GLOBALLY DECLARED VARIABLES
 app.citiesArray = ["Brooklyn", "New York", "Queens", "Bronx", "Staten Island"];           
+app.classArray = ["show-all", "art", "science", "history", "cultural", "childrens"];
+app.classIcons = ["", "fa-palette", "fa-atom", "fa-landmark", "fa-globe", "fa-child"];
 app.numCities = app.citiesArray.length - 1;
-app.classArray = ["art", "science", "history", "cultural", "childrens"];
-app.classIcons = ["fa-palette", "fa-atom", "fa-landmark", "fa-globe", "fa-child"];
 
 // VISIBILITY
-$('button, li, i, input[type="submit"]').addClass('pointer');
+$('button, label, i, input[type="submit"]').addClass('pointer');
 $('.scroll-up, .filter-bar').hide();
 
 //KEYWORDS TO FILTER AND ASSIGN MUSEUM TYPES
 app.strArray = [ 
+                [" "],
                 ["Art", "Galerie", "FIT", "Design", "Studio", "Guggenheim", "Illustration", "Collection", "Drawing", "Brooklyn Museum", "Media", "Moving Image", "Photography", "National Academy", "Nicholas Roerich", "Goethe-Institut", "Dahesh"],
                 ["Science", "Technology", "Transit", "Space", "Skyscraper", "Seaport"], 
                 ["History", "Heritage", "Memorial", "Historic", "Archival", "Archive", "Hall Of Fame", "Police", "Bowne", "Library", "Finance", "Alice Austen", "Defense", "Homestead", "Waterfront", "Alexander Hamilton", "Museum of the City", "Holocaust", "Fire", "Island", "Valentine-Varian", "Cortlandt", "Theodore Roosevelt", "Lighthouse", "Farm", "Old Stone", "Mansion", "Manor", "Rose Museum", "Vernon Hotel", "Fraunces", "Jackie Robinson", "Audubon "],
                 ["Culture", "Cultural", "Leo Baeck", "Yeshiva", "Americas", "Numismatic", "Hispanic", "Tenement", "Madame Tussauds", "Tolerance", "Anne Frank","Garibaldi-Meucci", "Jewish", "Asia", "Chinese", "Nordic", "Museo", "Ukrainian", "Italian", "Jazz", "Indian", "Wave Hill"],
-                ["Children's", "Discovery Times"],
-                [" "]
+                ["Children's", "Discovery Times"]
                 ];
 
-let cityIndex = 0; 
+let cityIndex = 0;  
+let typeFilter = app.strArray[0];
 
 // FUNCTIONS
-app.displayCity = () => {   
-    app.$cities.text(app.citiesArray[cityIndex]);
-}
-
+// WHEN USER CLICKS UP IN CITY LIST
 app.toggleCityUp = () => {   
     if (cityIndex > 0) {
         cityIndex--;
@@ -51,15 +49,22 @@ app.toggleCityUp = () => {
     app.displayCity();
 }
 
+// WHEN USER CLICKS DOWN IN CITY LIST
 app.toggleCityDown = () => {  
     if (cityIndex < app.numCities) {
-    cityIndex++;
+        cityIndex++;
     } else {
         cityIndex = 0;
     }
     app.displayCity();
 } 
 
+// AFTER USER TOGGLES THRU THE CITY LIST, DISPLAY THE NAME OF THE CITY (FROM ARRAY)
+app.displayCity = () => {   
+    app.$cities.text(app.citiesArray[cityIndex]);
+}
+
+// WHEN USER SUBMITS CHOICE OF CITY, MAKE A CALL TO THE API
 app.evaluateCity = () => {
     const selectedCity = app.citiesArray[cityIndex];
     app.getByCity(selectedCity);
@@ -86,38 +91,24 @@ app.getByCity = (borough) => {
 
 // ASSIGNS THE COLOUR TAB FOR EACH TYPE 
 app.assignType = function () { 
-    for (let n = 0; n<5; n++) {
+    for (let n = 1; n<6; n++) {
         $(app.strArray[n]).each(function(){
             $(".result-container:contains(" + this + ")").find('.icon').addClass(app.classArray[n]).find('.fas').addClass(app.classIcons[n]);
         })
     }
 }
 
-// USER INPUT FROM FILTER DROP DOWN MENU
-app.getUserFilter = () => { 
-    let str = "";
-    $('input[type="radio"]:checked').each(function() {
-        str = $(this).val();
-    });
-    return str
+app.getType = () => {
+    const idAttribute = $('input[type="radio"]:checked').attr("id"); 
+    for (let i = 0 ; i < app.classArray.length; i++) {
+        if (idAttribute === app.classArray[i]) {
+            typeFilter = app.strArray[i];
+        } 
+    }
 }
 
+// DISPLAYS RESULTS FOR THE SELECTED CITY OR FILTER OPTION
 app.displayResults = (museums) => {
-    const str = app.getUserFilter();
-    let typeFilter;
-    if (str === "Art"){
-        typeFilter = app.strArray[0];
-    } else if (str === "Science") {
-        typeFilter = app.strArray[1];
-    } else if (str === "History")  {
-        typeFilter = app.strArray[2];
-    } else if (str === "Cultural") {
-        typeFilter = app.strArray[3];
-    } else if (str === "Children's") {
-        typeFilter = app.strArray[4];
-    } else {
-        typeFilter = app.strArray[5];
-    }
     museums.forEach((museum) => {
         const name = museum.name;
         const address = museum.adress1;
@@ -136,8 +127,8 @@ app.displayResults = (museums) => {
                 <a class="tel" href="tel:${tel}"><i class="fas fa-phone" aria-hidden="true"></i> ${tel}</a> 
                 <a class="url" href="${url}" rel="external" target="_blank">Visit Website</a>
             </div>`; 
-        for (let n = 0; n<typeFilter.length; n++) {  
-            if (name.includes(typeFilter[n])) { 
+        for (let str = 0; str < typeFilter.length; str++) {  
+            if (name.includes(typeFilter[str])) { 
                 app.$searchResults.append(museumHtml);
             }
         }
@@ -160,13 +151,14 @@ app.init = () => {
     app.$toggleUp.on('click', app.toggleCityUp); 
     app.$toggleDown.on('click', app.toggleCityDown); 
     app.$cityForm.on('submit', function(e) {
-        e.preventDefault();
-        $('input[type="radio"]:checked').prop( "checked", false); //default user selection to Show All
+        e.preventDefault(); 
+        typeFilter = app.strArray[0];
+        $('input[type="radio"]:checked').prop("checked", false); //default user selection to Show All
         app.evaluateCity();
         app.$typeForm.show();
     })  
-    app.$selectType.change(function() {  
-        app.getUserFilter(); 
+    app.$selectType.change(function() {   
+        app.getType();
         app.evaluateCity();
     })
 }
